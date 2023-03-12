ee6783bd6fc806ad493d5f796bb52985bd83f21f
karna-diwakar-1990 <79453362+karna-diwakar-1990@users.noreply.github.com>
2023-03-13 00:33:15 +0530

    Added code to create screenshots and logs depending on the OS

--- utils/customlogger.py
+++ utils/customlogger.py
@@ -1,12 +1,20 @@
 import logging
+import platform
 
 
 class Logger:
     @staticmethod
     def logger():
-        logging.basicConfig(filename="./Logs/automation.log",
+
+        if platform.system() == "Windows":
+            logging.basicConfig(filename="Logs\\automation.log",
                             format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                             force=True)
+        elif platform.system() == "Linux":
+
+            logging.basicConfig(filename="./Logs/automation.log",
+                                format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                                force=True)
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         return logger
